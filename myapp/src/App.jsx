2484ca671ea4694c604e75ed4d7535f9087e514a
JesyGomez <julianycia.m@gmail.com>
2023-11-13 19:53:56 -0300

    borrando carpeta node

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -5,6 +5,8 @@ function App() {
 
   return (
     <>
+    <Product nombreProp="Televisor"/> 
+                <Product nombreProp="Smart"></Product>
     <Product>nombre="Televisor" descripcion="Smart, samsung 70 pulgadas" precio="400.000" sku="GLJ784" stock="50"</Product>
     </>
   )
